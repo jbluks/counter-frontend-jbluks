c0479105136bd5f6549c3c6ee311a85a1989e4cb
Jonathan <jon.bluks@gmail.com>
2018-08-20 10:24:33 -0700

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import axios from 'axios'
+
+class App extends Component {
+  state = {
+    counter: 0
+  }
+
+  componentDidMount() {
+    axios.get('/counter')
+      .then(response => {
+        this.setState({
+          counter: response.data.counter
+        })
+      })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        Counter: {this.state.counter}
+      </div>
+    );
+  }
+}
+
+export default App;
